e80f12687673aad2c3fee13976dae58ecef99b5e
Abutalif <abutaliph@gmail.com>
2025-09-02 16:52:25 +0500

    migration sql file update

--- resources/m0002-accounts-public-key-bindings.sql
+++ resources/m0002-accounts-public-key-bindings.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS account_public_key_bindings(
-    idx BIGSERIAL PRIMARY KEY ,
-    address BYTEA UNIQUE NOT NULL,
-    public_key BYTEA,
+    id BIGSERIAL PRIMARY KEY ,
+    address BYTEA NOT NULL,
+    public_key BYTEA NOT NULL,
     credential_index INT NOT NULL,
     key_index INT NOT NULL,
     is_simple_account BOOLEAN NOT NULL,
     active BOOLEAN NOT NULL
-)
+);
+
+CREATE INDEX IF NOT EXISTS idx_account_public_key_bindings_public_key ON account_public_key_bindings (public_key);
